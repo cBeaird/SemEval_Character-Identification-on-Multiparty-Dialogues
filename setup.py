d99d02d622955bb84132ccbe04ea362000284446
cBeaird <caseyit@gmail.com>
2017-09-06 17:34:28 -0400

    Initial Setup file to include required dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SemEval_Character-Identification-on-Multiparty-Dialogues',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/cBeaird/SemEval_Character-Identification-on-Multiparty-Dialogues',
+    license='MIT',
+    author='Casey, Chase, Brandon',
+    author_email='',
+    description='SemEval project Task 4: Conference Resolution',
+    install_requires=['conllu', 'nltk', 'tensorflow']
+)
