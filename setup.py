f597d0914d674d8e661cff2380e5f1ed28ab8f01
cBeaird <caseyit@gmail.com>
2017-09-07 09:09:55 -0400

    Conll package is not supported in python 3 setup will not install if running with python3

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+"""
+    Setup file with application requirements and licensing information
+    Casey Beaird
+"""
 from distutils.core import setup
+import sys
+
+__author__ = 'Casey Beaird'
+
+if sys.version_info[0] != 2:
+    v = sys.version
+    print('python version: {0}is not supported'.format(sys.version.split('\n')[0]))
+    exit(0)
 
 setup(
     name='SemEval_Character-Identification-on-Multiparty-Dialogues',
