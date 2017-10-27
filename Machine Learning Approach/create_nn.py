11903023ebde52783deb3578f288bc3e263d4054
cBeaird <caseyit@gmail.com>
2017-10-27 17:59:19 -0400

    little changes to the NN

--- Machine Learning Approach/create_nn.py
+++ Machine Learning Approach/create_nn.py
@@ -17,31 +17,33 @@ train_set, test_set = train_test_split(df,
                                        test_size=.2,
                                        random_state=42)  # Split the data into test and training data
 
-df = train_set.copy()                           # Make a copy of the training data
-feature_columns = create_feature_colums(df)     # Create feature columns
+df = train_set.copy()  # Make a copy of the training data
+feature_columns = create_feature_colums(df)  # Create feature columns
 
-dnn_clf = tf.estimator.DNNClassifier(feature_columns=feature_columns, # Deep NN Classifier
-                                     hidden_units=[300,100],
-                                     n_classes=400,
-                                     model_dir="nn_model")
+# dnn_clf = tf.estimator.DNNClassifier(feature_columns=feature_columns,  # Deep NN Classifier
+#                                      hidden_units=[300, 100],
+#                                      n_classes=401,
+#                                      model_dir="nn_model")
+lv = [str(x) for x in range(1, 401)]
+dnn_clf = tf.estimator.DNNClassifier(feature_columns=feature_columns,  # Deep NN Classifier
+                                     hidden_units=[300, 100],
+                                     n_classes=401)
 
-featureVectors = df.drop(['Entity_ID'], axis=1) # Features
-labels = df['Entity_ID']                        # Classes
+featureVectors = df.drop(['Entity_ID'], axis=1)  # Features
+labels = df['Entity_ID']  # Classes
 
 train_input_fn = tf.estimator.inputs.pandas_input_fn(
-      x=df,
-      y=labels,
-      batch_size=100,
-      num_epochs=100,
-      shuffle=True)
+    x=df,
+    y=labels,
+    batch_size=100,
+    num_epochs=100,
+    shuffle=True)
 
 dnn_clf.train(input_fn=train_input_fn, steps=2000)
 
-
-'''
 df = test_set.copy()
 labels = df['Entity_ID']
-featureVectors = df.drop(['Entity_ID'], axis=1)
+# featureVectors = df.drop(['Entity_ID'], axis=1)
 
 test_input_fn = tf.estimator.inputs.pandas_input_fn(
     x=featureVectors,
@@ -52,5 +54,5 @@ test_input_fn = tf.estimator.inputs.pandas_input_fn(
 
 accuracy_score = dnn_clf.evaluate(input_fn=test_input_fn)["accuracy"]
 
-'''
-#print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+
+# print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
