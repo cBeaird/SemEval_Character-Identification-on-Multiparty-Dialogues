ebe628bfc1a462c787053bbaed1a0999c71e0e2a
Brandon Watts <brandonwatts@outlook.com>
2017-12-03 17:50:19 -0500

    Fix Forest Mispelling

--- Random_Forrest.sh
+++ Random_Forrest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo Random Forrest Classifier
+echo Random Forest Classifier
 python Classifiers/conll_2_csv.py -m Classifiers/friends_word2vec_model -c datasets-None-8c441e63-e82a-48e6-b1a7-07811cc80cd8-friends.train.trial/friends.train.episode_delim.conll -f -o vectors.csv -w2v
 python Classifiers/Random_Forest/Random_Forest.py -tr vectors.csv -o Classifiers/Models/random-forest.pkl
-python Classifiers/Random_Forest/eval_rand_forrest.py -te vectors.csv -m Classifiers/Models/random-forest.pkl
+python Classifiers/Random_Forest/Random_Forest_Evaluation.py -te vectors.csv -m Classifiers/Models/random-forest.pkl
 rm Classifiers/Models/random-forest.pkl
